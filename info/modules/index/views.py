202156dfea92d77b8dca22e8c121a89706ccfcda
myfamilyhome <cwq75320673@163.com>
2019-08-12 20:47:50 +0800

    点击排行展示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -2,6 +2,8 @@ from . import index_blu
 from flask import render_template,current_app
 from flask import session
 from info.models import User
+from info.models import News
+from info import constants
 
 
 @index_blu.route("/")
@@ -14,7 +16,24 @@ def index():
             user = User.query.get(user_id)
         except Exception as e:
             current_app.logger.error(e)
-    return render_template("news/index.html", data={"user_info": user.to_dict() if user else None})
+    # 获取点击排行数据
+    news = None
+
+    try:
+        news = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
+    except Exception as e:
+        current_app.logger.error(e)
+
+    news_list = []
+    if news:
+        for new in news:
+            news_list.append(new.to_basic_dict())
+    data = {
+        "user_info": user.to_dict() if user else None,
+        "news_list": news_list
+    }
+
+    return render_template("news/index.html", data=data)
 
 
 @index_blu.route("/favicon.ico")
